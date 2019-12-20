5d5123a59d6aa760eab477a4fd6ac7d0f79ce760
munelear <munelear@gmail.com>
2019-12-19 23:33:59 -0600

    Add initial support for crewless ships
    Credit to scuba on the swgoh.help discord server
    for providing the formulas that yield correct GP
    values for all current crewless ships.
    
    Additional modifications will likely need to be
    made once ships with different ability counts
    are added to the game.

--- power/core/calcGalacticPower.js
+++ power/core/calcGalacticPower.js
@@ -18,9 +18,9 @@ module.exports = async ( units ) => {
             if( un.combatType === 1 ) { continue; }
             if( un.crew ) {
                 for( var cmem of un.crew ) {
-	                let u = units.filter(pr => pr.defId === cmem.unitId);
-	                cmem.gp = u[0].gp || 0;
-	                cmem.cp = calcCrewRating( cmem.gp, un );
+                  let u = units.filter(pr => pr.defId === cmem.unitId);
+                  cmem.gp = u[0].gp || 0;
+                  cmem.cp = calcCrewRating( cmem.gp, un );
                 }
             }
             un.gp = calcShipGP( un );
@@ -59,7 +59,11 @@ function calcModGP( modList, raw ) {
 
 function calcAbilityGP( abilityList, combatType ) {
     try {
-        var gpAbility = 0;
+        var gpTotals = {
+          gpAbility: 0,
+          gpContract: 0,
+          gpReinforcement: 0
+        };
         abilityList.forEach( a => {
             let iskill
             let otag
@@ -69,14 +73,14 @@ function calcAbilityGP( abilityList, combatType ) {
             }
 
             if( (a.tiers === 3 && combatType === 1) || ( otag && (a.id.includes('contract') || otag.includes('contract'))) ) {
-            	gpAbility += Number(gpTables.contractTable[(a.tier || 0)]);
+              gpTotals.gpContract += Number(gpTables.contractTable[(a.tier || 0)]);
             } else if( (a.tiers === 3 && combatType === 2) || (otag && (a.id.includes('hardware') || otag.includes('reinforcement'))) ) {
-            	gpAbility += Number(gpTables.reinforcementTable[(a.tier-1 || 0)]);
+              gpTotals.gpReinforcement += Number(gpTables.reinforcementTable[(a.tier-1 || 0)]);
             } else {
-            	gpAbility += a.tier === 8 && a.isZeta ? Number(gpTables.abilityTable[a.tier+1]) : Number(gpTables.abilityTable[(a.tier || 0)]);
+              gpTotals.gpAbility += a.tier === 8 && a.isZeta ? Number(gpTables.abilityTable[a.tier+1]) : Number(gpTables.abilityTable[(a.tier || 0)]);
             }
         });
-        return gpAbility;
+        return gpTotals;
     } catch(e) {
         console.error(e);
         return 0
@@ -115,11 +119,12 @@ function calcCharGP( unit ) {
 
         var gpModifier  = 1.5;
         var gpMod       = calcModGP( unit.mods );
-        var gpAbility   = calcAbilityGP( unit.skills, unit.combatType );
+        var {gpAbility, gpContract} = calcAbilityGP( unit.skills, unit.combatType );
         var gpGear      = calcGearGP( unit.gear, unit.equipped );
         var gpRarity    = gpTables.rarityTable[ unit.rarity ];
         var gpLevel     = gpTables.levelTable[ unit.level ];
         var relicGp     = 0;
+        gpAbility += gpContract;
 
         if( debug ) console.log( gpMod, gpAbility, gpGear, gpRarity, gpLevel )
 
@@ -156,29 +161,34 @@ function calcShipGP( unit ) {
         var gpRarity    = parseFloat(gpTables.rarityTable[ unit.rarity ]);
         var gpCrewSize  = parseFloat(gpTables.crewSizeTable[unit.crew.length]);
         var gpLevel     = parseFloat(gpTables.levelTable[ unit.level ]);
-        var gpAbility   = parseFloat(calcAbilityGP( unit.skills , unit.combatType));
+        var {gpAbility, gpReinforcement} = calcAbilityGP( unit.skills , unit.combatType);
         var gpModifier  = parseFloat(gpTables.multiplierTable[unit.rarity]);
 
         var gpCrewPower = 0;
         var gpShipPower = 0;
         var gpCrew = 0;
+        var gpTotal = 0;
 
         unit.crew.forEach( cmem => {
             gpCrewPower += parseFloat(cmem.cp);
             gpCrew += parseFloat(cmem.gp);
         });
 
-        gpCrew = gpCrew * gpModifier * gpCrewSize;
-        gpShipPower = (( gpCrew / 2 ) + (( gpLevel + gpAbility ) * 1.5));
-
-        // TODO - crewless ships
-        //const numOfAbilities = unit.skills.length;
-        //const gpPerAbilityModifier = gpTables.crewlessGPModiferPerAbilityTable[numOfAbilities];
+        if (unit.crew.length === 0) {
+          // TODO - update calculation when crewless ship with more than 3 abilities + reinforcement released?
+          //const numOfAbilities = 3;
+          //const gpPerAbilityModifier = gpTables.crewlessGPModiferPerAbilityTable[numOfAbilities];
+          gpCrew = (gpLevel * 3.5 + gpAbility * 5.74 + gpReinforcement * 1.61) * gpModifier;
+          gpTotal = (gpCrew + gpLevel + gpAbility + gpReinforcement) * 1.5;
+        } else {
+          gpCrew = gpCrew * gpModifier * gpCrewSize;
+          gpShipPower = (( gpCrew / 2 ) + (( gpLevel + gpAbility + gpReinforcement) * 1.5));
+          gpTotal = gpCrewPower + gpShipPower
+        }
 
         if( debug ) console.log( gpRarity, gpCrewSize, gpLevel, gpAbility, gpModifier, gpCrewPower, gpShipPower, gpCrew )
 
-        return Math.floor(gpCrewPower + gpShipPower);
-
+        return Math.floor(gpTotal);
     } catch(e) {
         console.error(e);
         return 0
